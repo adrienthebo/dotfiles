d38ed69edd04f8ca1b3868538dd29124bec99c7e
Adrien Thebo <adrien@lagrange-automation.io>
2025-01-07 16:07:26 -0800

    chore: bump lunarvim config

--- home/dot_config/lvim/config.lua
+++ home/dot_config/lvim/config.lua
@@ -3,9 +3,13 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
+lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["gT"] = ":BufferLineCyclePrev<CR>"
 
 lvim.plugins = {
+  { "terrastruct/d2-vim" },
   { "tpope/vim-surround" },
+  { 'towolf/vim-helm',       ft = 'helm' },
   {
     "zbirenbaum/copilot-cmp",
     event = "InsertEnter",
@@ -17,4 +21,128 @@ lvim.plugins = {
       end, 100)
     end,
   },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+      vim.defer_fn(function()
+        require('rainbow-delimiters.setup').setup {
+          --strategy = {
+          --  -- ...
+          --},
+          --query = {
+          --  -- ...
+          --},
+          highlight = {
+            'RainbowDelimiterRed',
+            'RainbowDelimiterYellow',
+            'RainbowDelimiterBlue',
+            'RainbowDelimiterOrange',
+            'RainbowDelimiterGreen',
+            'RainbowDelimiterViolet',
+            'RainbowDelimiterCyan',
+          },
+        }
+
+      end, 100)
+    end,
+  },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      lang = "go",
+      non_standalone = true,
+      -- configuration goes here
+    },
+  }
 }
+
+-- https://github.com/LunarVim/LunarVim/discussions/3940
+vim.opt.foldmethod = "expr" -- default is "normal"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- default is ""
+vim.opt.foldenable = false -- if this option is true and fold method option is other than normal, every time a document is opened everything will be folded.
+
+--------------------------------------------------------------------------------
+-- Language server configuration
+--------------------------------------------------------------------------------
+
+local lspconfig = require("lspconfig")
+
+-- pyright
+
+lspconfig.pyright.before_init = function(params, config)
+  local Path = require "plenary.path"
+  local venv = Path:new((config.root_dir:gsub("/", Path.path.sep)), ".venv")
+  if venv:joinpath("bin"):is_dir() then
+    config.settings.python.pythonPath = tostring(venv:joinpath("bin", "python"))
+  else
+    config.settings.python.pythonPath = tostring(venv:joinpath("Scripts", "python.exe"))
+  end
+end
+
+-- helm-ls configuration
+
+lspconfig.helm_ls.setup {
+  settings = {
+    ['helm-ls'] = {
+      yamlls = {
+        path = "yaml-language-server",
+      }
+    }
+  }
+}
+
+
+-- setup yamlls
+lspconfig.yamlls.setup {}
+
+
+-- below are keymapping as recommended by nvim-lspconfig
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
